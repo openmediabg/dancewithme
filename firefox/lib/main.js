f1c95ff70c79c1cab7542cb92dd0e32027443722
ignisf <bordjukov@gmail.com>
2013-06-27 15:10:39 +0300

    Implement a nsIContentPolicy that redirects to warning.html

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,47 +1,35 @@
-const {PageMod} = require("sdk/page-mod");
-const {data}      = require("sdk/self");
+var {data} = require("sdk/self");
+var {Class} = require('sdk/core/heritage');
+var {Unknown, Factory} = require('sdk/platform/xpcom');
+var {Cc, Ci} = require("chrome");
 
-const BAD_DOMAINS = [
-  'monitor.bg',
-  'telegraph.bg',
-  'politika.bg',
-  'europost.eu',
-  'europost.bg',
-  'borbabg.com',
-  'tv7.bg',
-  'news7.bg',
-  'super7.bg',
-  '3bay.bg',
-  'econ.bg',
-  'inews.bg',
-  'jenite.bg',
-  'div.bg',
-  'sporta.bg',
-  'fitwell.bg',
-  'sportuvaj.bg',
-  'peika.bg',
-  'kulinar.bg',
-  'sever.bg',
-  'get.bg',
-  'bnews.bg',
-  'bsport.bg',
-  'blife.bg',
-  'bpost.bg',
-  'bmobile.bg',
-  'blitz.bg',
-  'standartnews.com',
-  'struma.bg',
-  'marica.bg',
-  'vsekiden.com',
-  'novinar.bg'
-].map(function(site) {
-  return '*.' + site;
+var contractId = '@ignorepeevski.tumblr.com/dancewithme';
+
+var PeevskiPolicy = Class({
+  extends: Unknown,
+  interfaces: ['nsIContentPolicy'],
+  get wrappedJSObject() this,
+
+  shouldLoad: function(aContentType, aContentLocation, aRequestOrigin, aContext, aMimeTypeGuess, aExtra) {
+    result = Ci.nsIContentPolicy.ACCEPT;
+
+    // we should check for TYPE_SUBDOCUMENT as well if we want frames.
+    if ((Ci.nsIContentPolicy.TYPE_DOCUMENT == aContentType) &&
+      /.*monitor.bg.*/.test(aContentLocation.spec)) {
+
+      aContext.loadURI(data.url('warning.html'), aRequestOrigin);
+      result = Ci.nsIContentPolicy.REJECT_SERVER;
+    }
+    return result;
+  }
 });
 
-// Create a page mod
-// It will run a script whenever a ".org" URL is loaded
-// The script replaces the page contents with a message
-PageMod({
-  include: BAD_DOMAINS,
-  contentScriptFile: data.url("warning.js")
+var factory = Factory({
+  contract: contractId,
+  Component: PeevskiPolicy
 });
+
+categoryManager = Cc["@mozilla.org/categorymanager;1"]
+                      .getService(Ci.nsICategoryManager);
+
+categoryManager.addCategoryEntry('content-policy', 'ignorepeevski', contractId, false, false);
