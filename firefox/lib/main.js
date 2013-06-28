8a3de7a8e87de437a4ce3fba963ae1b9d147d996
ignisf <bordjukov@gmail.com>
2013-06-28 15:54:09 +0300

    Encompass the firefox extension in a function

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -5,63 +5,66 @@ var {Cc, Ci} = require("chrome");
 var {PageMod} = require('sdk/page-mod');
 var {checkForPeevskiDomain} = require('./blocked_domains');
 
-var domainExceptions = {};
+exports.main = function() {
+  console.log("shite");
+  var domainExceptions = {};
 
-/*
- * Firefox does not like the : symbol in resource:// urls so we work
- * around this by keeping the urls in an array and passing around their keys.
- */
-var requestedUrls = [];
+  /*
+   * Firefox does not like the : symbol in resource:// urls so we work
+   * around this by keeping the urls in an array and passing around their keys.
+   */
+  var requestedUrls = [];
 
-var addDomainException = function(url) {
-  var domain = checkForPeevskiDomain(url);
-  var date = new Date();
-  var time = date.getTime();
-  domainExceptions[domain] = time + 10 * 60 * 1000;
-}
+  var addDomainException = function(url) {
+    var domain = checkForPeevskiDomain(url);
+    var date = new Date();
+    var time = date.getTime();
+    domainExceptions[domain] = time + 10 * 60 * 1000;
+  }
 
-var checkForDomainException = function(domain) {
-  return(domainExceptions[domain] !== undefined && new Date().getTime() < domainExceptions[domain]);
-}
+  var checkForDomainException = function(domain) {
+    return(domainExceptions[domain] !== undefined && new Date().getTime() < domainExceptions[domain]);
+  }
 
-var contractId = '@ignorepeevski.tumblr.com/dancewithme';
+  var contractId = '@ignorepeevski.tumblr.com/dancewithme';
 
-var PeevskiPolicy = Class({
-  extends: Unknown,
-  interfaces: ['nsIContentPolicy'],
-  get wrappedJSObject() this,
+  var PeevskiPolicy = Class({
+    extends: Unknown,
+    interfaces: ['nsIContentPolicy'],
+    get wrappedJSObject() this,
 
-  shouldLoad: function(aContentType, aContentLocation, aRequestOrigin, aContext, aMimeTypeGuess, aExtra) {
-    var result = Ci.nsIContentPolicy.ACCEPT;
-    var url = aContentLocation.spec;
-    var peevskiDomain = checkForPeevskiDomain(url);
+    shouldLoad: function(aContentType, aContentLocation, aRequestOrigin, aContext, aMimeTypeGuess, aExtra) {
+      var result = Ci.nsIContentPolicy.ACCEPT;
+      var url = aContentLocation.spec;
+      var peevskiDomain = checkForPeevskiDomain(url);
 
-    if ((Ci.nsIContentPolicy.TYPE_DOCUMENT == aContentType) && peevskiDomain && !checkForDomainException(peevskiDomain)) {
-      var urlIndex = requestedUrls.push(url) - 1;
-      redirectUrl = data.url("warning.html?" + urlIndex);
+      if ((Ci.nsIContentPolicy.TYPE_DOCUMENT == aContentType) && peevskiDomain && !checkForDomainException(peevskiDomain)) {
+        var urlIndex = requestedUrls.push(url) - 1;
+        var redirectUrl = data.url("warning.html?" + urlIndex);
 
-      aContext.loadURI(redirectUrl, aRequestOrigin);
-      result = Ci.nsIContentPolicy.REJECT_SERVER;
+        aContext.loadURI(redirectUrl, aRequestOrigin);
+        result = Ci.nsIContentPolicy.REJECT_SERVER;
+      }
+      return result;
     }
-    return result;
-  }
-});
+  });
 
-var factory = Factory({
-  contract: contractId,
-  Component: PeevskiPolicy
-});
+  var factory = Factory({
+    contract: contractId,
+    Component: PeevskiPolicy
+  });
 
-var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-categoryManager.addCategoryEntry('content-policy', 'ignorepeevski', contractId, false, false);
+  var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
+  categoryManager.addCategoryEntry('content-policy', 'ignorepeevski', contractId, false, false);
 
-PageMod({
-  include: /resource:\/\/.*\/dancewithme\/data\/warning.html.*/,
-  contentScriptFile: data.url('warning.js'),
-  onAttach: function(worker) {
-    worker.port.on("allowCurrentUrl", function(urlIndex) {
-      addDomainException(requestedUrls[urlIndex]);
-      worker.port.emit("redirectTo", requestedUrls[urlIndex]);
-    });
-  }
-});
+  PageMod({
+    include: /resource:\/\/.*\/dancewithme\/data\/warning.html.*/,
+    contentScriptFile: data.url('warning.js'),
+    onAttach: function(worker) {
+      worker.port.on("allowCurrentUrl", function(urlIndex) {
+        addDomainException(requestedUrls[urlIndex]);
+        worker.port.emit("redirectTo", requestedUrls[urlIndex]);
+      });
+    }
+  });
+}
