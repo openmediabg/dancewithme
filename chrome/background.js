f3a4fceb6720fd49b2ae9560b2dabac7d61e5689
Evgeni Kunev <evgeni.kunev@gmail.com>
2013-06-30 18:27:43 +0300

    [#13] Fix bad indentation in chrome/background.js

--- chrome/background.js
+++ chrome/background.js
@@ -6,7 +6,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     if (blocking !== false) {
       var date = new Date();
       var time = date.getTime();
-                        var warning_page = blocking.reason == 'domain' ? 'warning.html' : 'warning_fb.html';
+      var warning_page = blocking.reason == 'domain' ? 'warning.html' : 'warning_fb.html';
       if (domainExceptions[blocking.url] === undefined || time > domainExceptions[blocking.url]) {
         return {redirectUrl: chrome.extension.getURL(warning_page) + '?' + encodeURIComponent(info.url)};
       }
