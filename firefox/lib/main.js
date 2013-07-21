5a3edfb4742e04108f6eb1752afa0f071c9b5db2
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-07-21 16:17:05 +0300

    Firefox support for facebook pages blocking

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,24 +1,24 @@
-var {data} = require("sdk/self");
-var {Class} = require('sdk/core/heritage');
-var {Unknown, Factory} = require('sdk/platform/xpcom');
-var {Cc, Ci} = require("chrome");
-var {PageMod} = require('sdk/page-mod');
+var {data}                  = require("sdk/self");
+var {Class}                 = require('sdk/core/heritage');
+var {Unknown, Factory}      = require('sdk/platform/xpcom');
+var {Cc, Ci}                = require("chrome");
+var {PageMod}               = require('sdk/page-mod');
 var {checkForPeevskiDomain} = require('./blocked_domains');
-var base64url = require('./base64url');
+var base64url               = require('./base64url');
 
 exports.main = function() {
   var domainExceptions = {};
 
   var addDomainException = function(url) {
-    var domain = checkForPeevskiDomain(url);
-    var date = new Date();
-    var time = date.getTime();
-    domainExceptions[domain] = time + 10 * 60 * 1000;
-  }
+    var blocked = checkForPeevskiDomain(url);
+    if (blocked) {
+      domainExceptions[blocked.url] = new Date().getTime() + 10 * 60 * 1000;
+    }
+  };
 
   var checkForDomainException = function(domain) {
-    return(domainExceptions[domain] !== undefined && new Date().getTime() < domainExceptions[domain]);
-  }
+    return domainExceptions[domain] !== undefined && new Date().getTime() < domainExceptions[domain];
+  };
 
   var contractId = '@ignorepeevski.tumblr.com/dancewithme';
 
@@ -28,12 +28,13 @@ exports.main = function() {
     get wrappedJSObject() this,
 
     shouldLoad: function(aContentType, aContentLocation, aRequestOrigin, aContext, aMimeTypeGuess, aExtra) {
-      var result = Ci.nsIContentPolicy.ACCEPT;
-      var url = aContentLocation.spec;
-      var peevskiDomain = checkForPeevskiDomain(url);
+      var result  = Ci.nsIContentPolicy.ACCEPT;
+      var url     = aContentLocation.spec;
+      var blocked = checkForPeevskiDomain(url);
 
-      if ((Ci.nsIContentPolicy.TYPE_DOCUMENT == aContentType) && peevskiDomain && !checkForDomainException(peevskiDomain)) {
-        let redirectUrl = data.url("warning.html") + "?" + base64url.urlSafeEncode(url);
+      if ((Ci.nsIContentPolicy.TYPE_DOCUMENT == aContentType) && blocked && !checkForDomainException(blocked.url)) {
+        let warningPage = blocked.reason === 'domain' ? 'warning.html' : 'warning_fb.html';
+        let redirectUrl = data.url(warningPage) + "?" + base64url.urlSafeEncode(url);
         aContext.loadURI(redirectUrl, aRequestOrigin);
         result = Ci.nsIContentPolicy.REJECT_SERVER;
       }
@@ -50,7 +51,7 @@ exports.main = function() {
   categoryManager.addCategoryEntry('content-policy', 'ignorepeevski', contractId, false, false);
 
   PageMod({
-    include: /resource:\/\/.*\/dancewithme\/data\/warning\.html.*/,
+    include: /resource:\/\/.*\/dancewithme\/data\/warning(_fb)?\.html.*/,
     contentScriptFile: [data.url('base64decode.js'),
                         data.url('warning.js')],
     onAttach: function(worker) {
@@ -61,4 +62,4 @@ exports.main = function() {
       });
     }
   });
-}
+};
