418b78269fe75d3b2af99f9d138f7a7ccdd9bf5b
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-07-21 15:02:42 +0300

    Fix style (camelCase instead of snake_case, === instead of ==, etc)

--- chrome/background.js
+++ chrome/background.js
@@ -3,12 +3,12 @@ var domainExceptions = {};
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
     var blocking = checkForPeevskiDomain(info.url);
-    if (blocking !== false) {
-      var date = new Date();
-      var time = date.getTime();
-      var warning_page = blocking.reason == 'domain' ? 'warning.html' : 'warning_fb.html';
+    if (blocking) {
+      var date        = new Date();
+      var time        = date.getTime();
+      var warningPage = blocking.reason === 'domain' ? 'warning.html' : 'warning_fb.html';
       if (domainExceptions[blocking.url] === undefined || time > domainExceptions[blocking.url]) {
-        return {redirectUrl: chrome.extension.getURL(warning_page) + '?' + encodeURIComponent(info.url)};
+        return {redirectUrl: chrome.extension.getURL(warningPage) + '?' + encodeURIComponent(info.url)};
       }
     }
     return null;
@@ -28,13 +28,13 @@ chrome.webRequest.onBeforeRequest.addListener(
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.allowCurrentUrl) {
-      var url    = request.allowCurrentUrl;
+      var url      = request.allowCurrentUrl;
       var blocking = checkForPeevskiDomain(url);
-      var date   = new Date();
-      var time   = date.getTime();
+      var date     = new Date();
+      var time     = date.getTime();
       domainExceptions[blocking.url] = time + 10 * 60 * 1000;
 
-      chrome.tabs.getSelected(null, function (tab) {
+      chrome.tabs.getSelected(null, function(tab) {
         chrome.tabs.update(tab.id, {url: url});
       });
     }
